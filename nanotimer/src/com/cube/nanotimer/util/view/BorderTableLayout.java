88fcbd96444637ae8ec91b850b625431269ce384
Morgoth890 <inferno_feanor@hotmail.com>
2014-08-07 22:13:10 +0200

    remove useless border table code and use xml parameters

--- nanotimer/src/com/cube/nanotimer/util/view/BorderTableLayout.java
+++ nanotimer/src/com/cube/nanotimer/util/view/BorderTableLayout.java
@@ -1,6 +1,7 @@
 package com.cube.nanotimer.util.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -13,7 +14,6 @@ public class BorderTableLayout extends TableLayout {
 
   private Paint paint = new Paint();
   private Rect rect = new Rect();
-  private LayoutParams params = new LayoutParams();
 
   private Integer borderColor;
   private int borderSize;
@@ -25,11 +25,12 @@ public class BorderTableLayout extends TableLayout {
 
   public BorderTableLayout(Context context, AttributeSet attrs) {
     super(context, attrs);
-    borderColor = getResources().getColor(R.color.neon);
-    borderSize = 2;
-
-    int margin = Math.min(borderSize / 2, 1);
-    params.setMargins(margin, margin, margin, margin);
+    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BorderTableLayout);
+    if (a.hasValue(R.styleable.BorderTableLayout_borderColor)) {
+      // Cell has a border
+      borderColor = a.getColor(R.styleable.BorderTableLayout_borderColor, R.color.neon);
+      borderSize = a.getInt(R.styleable.BorderTableLayout_borderSize, 2);
+    }
 
     setWillNotDraw(false);
   }
@@ -47,31 +48,14 @@ public class BorderTableLayout extends TableLayout {
     canvas.drawRect(0, 0, borderSize, getMeasuredHeight(), paint); // left line
     canvas.drawRect(getMeasuredWidth() - borderSize, 0, getMeasuredWidth(), getMeasuredHeight(), paint); // right line
     canvas.drawRect(0, getMeasuredHeight() - borderSize, getMeasuredWidth(), getMeasuredHeight(), paint); // bottom line
-    // draw row lines
+    // draw row inner lines
     for (int row = 0; row < getChildCount() - 1; row++) {
       TableRow tr = (TableRow) getChildAt(row);
-      /*if (tr.getVisibility() == View.GONE) {
-        continue;
-      }
-      for (int col = 0; col < tr.getChildCount(); col++) {
-        View v = tr.getChildAt(col);
-        if (v.getVisibility() == View.GONE || "noborder".equals(v.getTag())) {
-          continue;
-        }
-        // TODO : take care of v visibility (could be GONE)
-        v.getGlobalVisibleRect(rect);
-
-//        int left = rect.left - tableLeft;
-//        int top = rect.top - tableTop;
-//        int width = v.getMeasuredWidth();
-//        int height = v.getMeasuredHeight();
-//        drawBorders(canvas, left, top, width, height);
-      }*/
       tr.getGlobalVisibleRect(rect);
       int bottom = rect.bottom - tableTop;
       canvas.drawRect(0, bottom, getMeasuredWidth(), bottom + borderSize, paint);
     }
-    // draw column lines
+    // draw column inner lines
     if (getChildCount() > 0) {
       TableRow tr = (TableRow) getChildAt(0);
       for (int col = 0; col < tr.getChildCount() - 1; col++) {
@@ -81,17 +65,6 @@ public class BorderTableLayout extends TableLayout {
       }
     }
     super.onDraw(canvas);
-  }
-
-  private void drawBorders(Canvas canvas, int x, int y, int canvasWidth, int canvasHeight) {
-    // top
-    canvas.drawRect(x - borderSize, y, x + canvasWidth + borderSize, y - borderSize, paint);
-    // left
-    canvas.drawRect(x, y - borderSize, x - borderSize, y + canvasHeight + borderSize, paint);
-    // right
-    canvas.drawRect(x + canvasWidth, y - borderSize, x + canvasWidth + borderSize, y + canvasHeight + borderSize, paint);
-    // bottom
-    canvas.drawRect(x - borderSize, y + canvasHeight, x + canvasWidth + borderSize, y + canvasHeight + borderSize, paint);
   }
 
 }
