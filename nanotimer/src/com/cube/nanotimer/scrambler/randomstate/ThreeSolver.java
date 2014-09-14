4bec3c7ca0e31567dd75090c28005c8c12ca651c
Morgoth890 <inferno_feanor@hotmail.com>
2014-09-14 21:06:27 +0200

    random-state solver continuation

--- nanotimer/src/com/cube/nanotimer/scrambler/randomstate/ThreeSolver.java
+++ nanotimer/src/com/cube/nanotimer/scrambler/randomstate/ThreeSolver.java
@@ -2,6 +2,38 @@ package com.cube.nanotimer.scrambler.randomstate;
 
 public class ThreeSolver {
 
+  // Cubies numbering:
+  //
+  //       U              D        U (mid-layer)
+  // #############  #############  #############
+  // # 1 # 3 # 4 #  # 6 # 9 # 7 #  # 7 #   # 6 #
+  // #############  #############  #############
+  // # 4 #   # 2 #  # 12#   # 10#  #   #   #   #
+  // #############  #############  #############
+  // # 2 # 1 # 3 #  # 5 # 11# 8 #  # 8 #   # 5 #
+  // #############  #############  #############
+
+  static enum Move {
+    U(new byte[] { 2, 3, 4, 1, 5, 6, 7 }, new byte[] { 0, 0, 0, 0, 0, 0, 0 }, new byte[] { 2, 3, 4, 1, 5, 6, 7, 8, 9, 10, 11 },  new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }),
+    D(new byte[] { 1, 2, 3, 4, 8, 5, 6 }, new byte[] { 0, 0, 0, 0, 0, 0, 0 }, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 12, 9, 10 },  new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }),
+    R(new byte[] { 1, 2, 7, 3, 5, 6, 8 }, new byte[] { 0, 0, 2, 1, 0, 0, 1 }, new byte[] { 1, 5, 3, 4, 10, 2, 7, 8, 9, 6, 11 },  new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }),
+    L(new byte[] { 5, 1, 3, 4, 5, 2, 7 }, new byte[] { 2, 1, 0, 0, 1, 2, 0 }, new byte[] { 1, 2, 3, 7, 5, 6, 12, 4, 9, 10, 11 }, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }),
+    F(new byte[] { 1, 6, 2, 4, 5, 7, 3 }, new byte[] { 0, 2, 1, 0, 0, 1, 2 }, new byte[] { 8, 2, 3, 4, 1, 6, 7, 9, 5, 10, 11 },  new byte[] { 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0 }),
+    B(new byte[] { 4, 2, 3, 8, 1, 6, 7 }, new byte[] { 1, 0, 0, 2, 2, 0, 0 }, new byte[] { 1, 2, 6, 4, 5, 11, 3, 8, 9, 10, 7 },  new byte[] { 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1 });
+
+    Move (byte[] corPerm, byte[] corOrient, byte[] edgPerm, byte[] edgOrient) {
+      this.corPerm = corPerm;
+      this.corOrient = corOrient;
+      this.edgPerm = edgPerm;
+      this.edgOrient = edgOrient;
+    }
+
+    byte[] corPerm;
+    byte[] corOrient;
+    byte[] edgPerm;
+    byte[] edgOrient;
+  }
+
   public static final int N_CORNER_PERMUTATIONS = 40320;
   public static final int N_CORNER_ORIENTATIONS = 2187;
 
@@ -24,9 +56,6 @@ public class ThreeSolver {
   private static int[][] transitUDEdgePermutation;
   private static int[][] transitEdgeOrientation;
 
-  private static String[][] moves1; // phase 1 moves
-  private static String[][] moves2; // phase 2 moves
-
   static class CubeState {
     public byte[] cornerPermutations = new byte[7];
     public byte[] cornerOrientations = new byte[7];
@@ -35,27 +64,9 @@ public class ThreeSolver {
   }
 
   static {
-    moves1 = new String[][] {
-        { "U", "U2", "U'" },
-        { "D", "D2", "D'" },
-        { "R", "R2", "R'" },
-        { "L", "L2", "L'" },
-        { "F", "F2", "F'" },
-        { "B", "B2", "B'" },
-    };
-
-    moves2 = new String[][] {
-        { "U", "U2", "U'" },
-        { "D", "D2", "D'" },
-        { "R2" },
-        { "L2" },
-        { "F2" },
-        { "B2" },
-    };
-
-    // ###########
-    // # Pruning #
-    // ###########
+    // ##################
+    // # Pruning tables #
+    // ##################
 
     // Phase 1
     pruningCornerOrientation = new int[N_CORNER_ORIENTATIONS];
@@ -69,18 +80,47 @@ public class ThreeSolver {
     pruningEEdgePermutation = new int[N_E_EDGE_PERMUTATIONS];
     pruningUDEdgePermutation = new int[N_U_D_EDGE_PERMUTATIONS];
 
-    // ##############
-    // # Transition #
-    // ##############
+    // #####################
+    // # Transition tables #
+    // #####################
 
+    Move[] moves = Move.values();
     // Phase 1
-    transitCornerOrientation = new int[N_CORNER_ORIENTATIONS][moves1.length]; // TODO : see if best to have moves1 size (18) or to just have the first or each move (size 6) and apply moves twice or trice to do "2" and "'"
-    transitEdgeOrientation = new int[N_EDGE_ORIENTATIONS][moves1.length];
+    transitCornerOrientation = new int[N_CORNER_ORIENTATIONS][moves.length];
+    for (int i = 0; i < transitCornerOrientation.length; i++) {
+      byte[] state = IndexConvertor.unpackCornerOrientation(i);
+      for (int j = 0; j < moves.length; j++) {
+        transitCornerOrientation[i][j] = IndexConvertor.packCornerOrientation(getOrientResult(state, moves[j].corOrient, (byte) 3));
+      }
+    }
+
+    transitEdgeOrientation = new int[N_EDGE_ORIENTATIONS][moves.length];
+    for (int i = 0; i < transitEdgeOrientation.length; i++) {
+      byte[] state = IndexConvertor.unpackEdgeOrientation(i);
+      for (int j = 0; j < moves.length; j++) {
+        transitEdgeOrientation[i][j] = IndexConvertor.packEdgeOrientation(getOrientResult(state, moves[j].edgOrient, (byte) 2));
+      }
+    }
 
     // Phase 2
-    transitCornerPermutation = new int[N_CORNER_PERMUTATIONS][moves2.length];
-    transitEEdgePermutation = new int[N_E_EDGE_PERMUTATIONS][moves2.length];
-    transitUDEdgePermutation = new int[N_U_D_EDGE_PERMUTATIONS][moves2.length];
+    transitCornerPermutation = new int[N_CORNER_PERMUTATIONS][moves.length];
+    transitEEdgePermutation = new int[N_E_EDGE_PERMUTATIONS][moves.length];
+    transitUDEdgePermutation = new int[N_U_D_EDGE_PERMUTATIONS][moves.length];
+  }
+
+  private static byte[] getPermResult(byte[] state, byte[] permIndices) {
+    byte[] result = new byte[state.length];
+    for (int i = 0; i < result.length; i++) {
+      result[i] = state[permIndices[i]];
+    }
+    return result;
+  }
+
+  private static byte[] getOrientResult(byte[] state, byte[] orientIndices, byte nDifferentValues) {
+    for (int i = 0; i < state.length; i++) {
+      state[i] += (orientIndices[i] % nDifferentValues);
+    }
+    return state;
   }
 
   public String[] getSolution(CubeState cubeState) {
