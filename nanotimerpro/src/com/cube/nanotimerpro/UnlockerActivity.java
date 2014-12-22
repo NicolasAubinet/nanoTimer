494dd5421311079fc13f5d6d773ddd81d928d6bc
Morgoth890 <inferno_feanor@hotmail.com>
2014-12-22 21:04:49 +0100

    add message to pro version if the free version is not recent enough

--- nanotimerpro/src/com/cube/nanotimerpro/UnlockerActivity.java
+++ nanotimerpro/src/com/cube/nanotimerpro/UnlockerActivity.java
@@ -3,7 +3,9 @@ package com.cube.nanotimerpro;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 public class UnlockerActivity extends Activity {
 
   private static final String NANOTIMERFREE_PACKAGE = "com.cube.nanotimer";
+  private static final int MIN_FREE_VERSION_CODE = 18;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,45 +30,66 @@ public class UnlockerActivity extends Activity {
     super.onResume();
 
     if (isAppInstalled(NANOTIMERFREE_PACKAGE)) {
-      ((TextView) findViewById(R.id.tvMainTitle)).setText(R.string.nanotimer_unlocked);
-      findViewById(R.id.tvAction).setVisibility(View.VISIBLE);
-      ((TextView) findViewById(R.id.tvAction)).setText(R.string.open_nanotimer);
-
-      Button buAction = (Button) findViewById(R.id.buAction);
-      buAction.setText(R.string.open);
-      buAction.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          Intent launchIntent = getPackageManager().getLaunchIntentForPackage(NANOTIMERFREE_PACKAGE);
-          if (launchIntent != null) {
-            startActivity(launchIntent);
-          } else {
-            Toast.makeText(UnlockerActivity.this, R.string.could_not_launch_nanotimer, Toast.LENGTH_LONG).show();
-          }
-        }
-      });
+      if (getAppVersionCode(NANOTIMERFREE_PACKAGE) >= MIN_FREE_VERSION_CODE) {
+        initValidScreen();
+      } else {
+        initVersionTooOldScreen();
+      }
     } else {
-      ((TextView) findViewById(R.id.tvMainTitle)).setText(R.string.nanotimer_should_be_installed);
-      findViewById(R.id.tvAction).setVisibility(View.GONE);
-
-      Button buAction = (Button) findViewById(R.id.buAction);
-      buAction.setText(R.string.go_to_play_store);
-      buAction.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          Intent storeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + NANOTIMERFREE_PACKAGE));
-          if (getPackageManager().queryIntentActivities(storeIntent, 0).size() > 0) {
-            try {
-              startActivity(storeIntent);
-            } catch (ActivityNotFoundException e) {
-              Toast.makeText(UnlockerActivity.this, R.string.could_not_launch_market, Toast.LENGTH_LONG).show();
-            }
-          } else {
-            Toast.makeText(UnlockerActivity.this, R.string.could_not_find_market, Toast.LENGTH_LONG).show();
-          }
-        }
-      });
+      initNotInstalledScreen();
     }
+  }
+
+  private void initValidScreen() {
+    ((TextView) findViewById(R.id.tvMainTitle)).setText(R.string.nanotimer_unlocked);
+    findViewById(R.id.tvAction).setVisibility(View.VISIBLE);
+    ((TextView) findViewById(R.id.tvAction)).setText(R.string.open_nanotimer);
+
+    Button buAction = (Button) findViewById(R.id.buAction);
+    buAction.setText(R.string.open);
+    buAction.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(NANOTIMERFREE_PACKAGE);
+        if (launchIntent != null) {
+          startActivity(launchIntent);
+        } else {
+          Toast.makeText(UnlockerActivity.this, R.string.could_not_launch_nanotimer, Toast.LENGTH_LONG).show();
+        }
+      }
+    });
+  }
+
+  private void initVersionTooOldScreen() {
+    ((TextView) findViewById(R.id.tvMainTitle)).setText(R.string.version_too_old);
+    findViewById(R.id.tvAction).setVisibility(View.GONE);
+    setActionButtonToPlayStorePage();
+  }
+
+  private void initNotInstalledScreen() {
+    ((TextView) findViewById(R.id.tvMainTitle)).setText(R.string.nanotimer_should_be_installed);
+    findViewById(R.id.tvAction).setVisibility(View.GONE);
+    setActionButtonToPlayStorePage();
+  }
+
+  private void setActionButtonToPlayStorePage() {
+    Button buAction = (Button) findViewById(R.id.buAction);
+    buAction.setText(R.string.go_to_play_store);
+    buAction.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent storeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + NANOTIMERFREE_PACKAGE));
+        if (getPackageManager().queryIntentActivities(storeIntent, 0).size() > 0) {
+          try {
+            startActivity(storeIntent);
+          } catch (ActivityNotFoundException e) {
+            Toast.makeText(UnlockerActivity.this, R.string.could_not_launch_market, Toast.LENGTH_LONG).show();
+          }
+        } else {
+          Toast.makeText(UnlockerActivity.this, R.string.could_not_find_market, Toast.LENGTH_LONG).show();
+        }
+      }
+    });
   }
 
   private boolean isAppInstalled(String packageName) {
@@ -77,13 +101,17 @@ public class UnlockerActivity extends Activity {
       installed = false;
     }
     return installed;
+  }
 
-    // OR (other way to check)
-//    List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-//    boolean installed = list.size() > 0;
-
-    // OR (other way to check)
-//    boolean installed = (i.resolveActivityInfo(getPackageManager(), 0) != null);
+  private int getAppVersionCode(String packageName) {
+    int version;
+    try {
+      PackageInfo packageInfo = getPackageManager().getPackageInfo(packageName, 0);
+      version = packageInfo.versionCode;
+    } catch (NameNotFoundException e) {
+      return -1;
+    }
+    return version;
   }
 
 }
