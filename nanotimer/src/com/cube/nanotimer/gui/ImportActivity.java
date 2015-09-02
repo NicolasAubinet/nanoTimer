1b45ec0f8bfd0259b6d59f9e1eebfa1014b57016
Morgoth890 <inferno_feanor@hotmail.com>
2015-09-02 21:18:28 +0200

    Improve file browser component

--- nanotimer/src/com/cube/nanotimer/gui/ImportActivity.java
+++ nanotimer/src/com/cube/nanotimer/gui/ImportActivity.java
@@ -3,17 +3,41 @@ package com.cube.nanotimer.gui;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import com.ankhsoft.filebrowser.FileBrowser;
+import com.ankhsoft.filebrowser.OnFileSelectedListener;
 import com.cube.nanotimer.R;
+import com.cube.nanotimer.util.helper.DialogUtils;
+
+import java.io.File;
 
 public class ImportActivity extends Activity {
+  private FileBrowser fileBrowser;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.import_screen);
+
+    initViews();
   }
 
   private void initViews() {
+    fileBrowser = (FileBrowser) findViewById(R.id.fileBrowser);
+    fileBrowser.setOnFileSelectedListener(new OnFileSelectedListener() {
+      @Override
+      public void onFileSelected(File parFile) {
+        DialogUtils.showInfoMessage(ImportActivity.this, "Selected file: " + parFile.getName());
+      }
+    });
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (fileBrowser.isRootFolder(fileBrowser.getCurrentFolder())) {
+      super.onBackPressed();
+    } else {
+      fileBrowser.moveToParent();
+    }
   }
 
   @Override
