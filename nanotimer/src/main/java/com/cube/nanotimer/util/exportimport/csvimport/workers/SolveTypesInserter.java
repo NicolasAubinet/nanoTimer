171f60be4cffda108ad40e18be9f6040e546e3e1
Morgoth890 <inferno_feanor@hotmail.com>
2017-06-28 19:22:59 +0200

    Fix crash that happened for solve types that have no scramble types

--- nanotimer/src/main/java/com/cube/nanotimer/util/exportimport/csvimport/workers/SolveTypesInserter.java
+++ nanotimer/src/main/java/com/cube/nanotimer/util/exportimport/csvimport/workers/SolveTypesInserter.java
@@ -10,6 +10,7 @@ import com.cube.nanotimer.util.exportimport.ErrorListener;
 import com.cube.nanotimer.util.exportimport.csvimport.ImportResultListener;
 import com.cube.nanotimer.util.exportimport.csvimport.ImportTimesData;
 import com.cube.nanotimer.vo.CubeType;
+import com.cube.nanotimer.vo.ScrambleType;
 import com.cube.nanotimer.vo.SolveType;
 
 import java.util.List;
@@ -34,11 +35,12 @@ public class SolveTypesInserter extends AsyncTask<List<SolveType>, Void, String>
     List<SolveType> missingSolveTypes = missingSolveTypesParams[0];
     boolean addedSpecialScrambleType = false;
 
-    for (SolveType missingSolveType : missingSolveTypes){
-      if (!missingSolveType.getScrambleType().isDefault()) {
+    for (SolveType missingSolveType : missingSolveTypes) {
+      ScrambleType scrambleType = missingSolveType.getScrambleType();
+      if (scrambleType != null && !scrambleType.isDefault()) {
         CubeType cubeType = CubeType.getCubeType(missingSolveType.getCubeTypeId());
         if (cubeType != null) {
-          addedSpecialScrambleType |= cubeType.addUsedScrambleType(missingSolveType.getScrambleType());
+          addedSpecialScrambleType |= cubeType.addUsedScrambleType(scrambleType);
         }
       }
       App.INSTANCE.getService().getProviderAccess().addSolveType(missingSolveType);
